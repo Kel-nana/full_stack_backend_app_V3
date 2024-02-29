414f0710482018829b1ff0f86a7c3a3885345421
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-02-29 19:06:11 +0300

    add controller method(getting data)

--- src/main/java/com/kel/full_stack_backend_app_V3/controller/UserController.java
+++ src/main/java/com/kel/full_stack_backend_app_V3/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
         return userRepository.save(newUser);
     }
 
-    @GetMapping("/user")
+    @GetMapping("/users")
     List<User> getAllUsers() {
         return userRepository.findAll();
     }
