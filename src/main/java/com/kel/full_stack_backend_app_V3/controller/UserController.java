673e00a81697503f3cd6b80c5b2f9850807f539e
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-02-29 18:02:11 +0300

    add user controller posting data

--- src/main/java/com/kel/full_stack_backend_app_V3/controller/UserController.java
+++ src/main/java/com/kel/full_stack_backend_app_V3/controller/UserController.java
@@ -1,4 +1,20 @@
 package com.kel.full_stack_backend_app_V3.controller;
 
+import com.kel.full_stack_backend_app_V3.model.User;
+import com.kel.full_stack_backend_app_V3.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
 }
