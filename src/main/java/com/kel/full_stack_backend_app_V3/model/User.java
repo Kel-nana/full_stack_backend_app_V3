3f0ef44512b776690d47d29d5c799b089a44b8c9
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-02-29 16:18:26 +0300

    use lombok for getter & setters

--- src/main/java/com/kel/full_stack_backend_app_V3/model/User.java
+++ src/main/java/com/kel/full_stack_backend_app_V3/model/User.java
@@ -3,7 +3,11 @@ package com.kel.full_stack_backend_app_V3.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class User {
 
@@ -15,35 +19,4 @@ public class User {
     private String name;
     private String email;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
