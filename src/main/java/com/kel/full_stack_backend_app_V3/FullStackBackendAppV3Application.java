b0b5b239b58119d5959d16188592bf9374954ab7
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-02-29 15:36:27 +0300

    initial commit

---
+++ src/main/java/com/kel/full_stack_backend_app_V3/FullStackBackendAppV3Application.java
@@ -0,0 +1,13 @@
+package com.kel.full_stack_backend_app_V3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackBackendAppV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackBackendAppV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kel/full_stack_backend_app_V3/FullStackBackendAppV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kel.full_stack_backend_app_V3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackBackendAppV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
