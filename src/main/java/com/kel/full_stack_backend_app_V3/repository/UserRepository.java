a90e6ff6dcffb26e36c009ba1fb12aba723ab007
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-02-29 16:11:35 +0300

    Extending JpaRepository to user

--- src/main/java/com/kel/full_stack_backend_app_V3/repository/UserRepository.java
+++ src/main/java/com/kel/full_stack_backend_app_V3/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.kel.full_stack_backend_app_V3.repository;
 
-public interface UserRepository {
+import com.kel.full_stack_backend_app_V3.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
