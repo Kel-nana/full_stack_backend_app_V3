84d8d1261db0cf566e3ddbda4af318843115427f
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-01 04:49:39 +0300

    connect to frontend

--- src/main/java/com/kel/full_stack_backend_app_V3/controller/UserController.java
+++ src/main/java/com/kel/full_stack_backend_app_V3/controller/UserController.java
@@ -3,14 +3,12 @@ package com.kel.full_stack_backend_app_V3.controller;
 import com.kel.full_stack_backend_app_V3.model.User;
 import com.kel.full_stack_backend_app_V3.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class UserController {
 
     @Autowired
