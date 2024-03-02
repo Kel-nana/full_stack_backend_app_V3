c30ae7790833bd769244715069e6dea5aa1872d5
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-02 20:45:58 +0300

    add getuser, edituser and deleteuser controllers

--- src/main/java/com/kel/full_stack_backend_app_V3/controller/UserController.java
+++ src/main/java/com/kel/full_stack_backend_app_V3/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.kel.full_stack_backend_app_V3.controller;
 
+import com.kel.full_stack_backend_app_V3.exception.UserNotFoundException;
 import com.kel.full_stack_backend_app_V3.model.User;
 import com.kel.full_stack_backend_app_V3.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("http://localhost:3000/")
@@ -21,6 +23,34 @@ public class UserController {
 
     @GetMapping("/users")
     List<User> getAllUsers() {
+
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                })
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) {
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "Deleted User "+id+" successfully.";
     }
 }
